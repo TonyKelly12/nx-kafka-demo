8774549938a8417110b86b5211906d7e4c070b73
TonyKelly12 <tonykelly1286@gmail.com>
2021-08-03 23:22:57 -0500

    initial commit

---
+++ apps/demos/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'demo-projects-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'demos';
+}
